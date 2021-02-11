6e6b7b26298ee2288d476c5a5b8082e4cf08b1ed
Roy Brez <rbrbrbrb8@gmail.com>
2021-02-11 09:37:24 +0200

    starting login page

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+app.use(express.static(path.join(__dirname,'static')));
+
+
 
 
 const logger = (req,res,next) =>{
