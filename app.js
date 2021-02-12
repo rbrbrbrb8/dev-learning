f1458a5f3e32cce8fb674e9207afd752b7172857
Roy Brez <rbrbrbrb8@gmail.com>
2021-02-13 01:02:15 +0200

    major progress in login page

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+
+// app.all('/vendors/*',(req,res)=>{
+//   res.status(403).json({msg: "permission denied"});
+// });
+// app.all('/scripts/*',(req,res) =>{
+//   res.status(403).json({msg: "permission denied"});
+// });
+
 app.use(express.static(path.join(__dirname,'static')));
 
 
@@ -13,13 +21,28 @@ const logger = (req,res,next) =>{
   next();
 }
 
+
+
 app.use(logger);
 
-app.use(express.static(path.join(__dirname,'static')));
 
-// app.get('/', (req, res) => {
-//   res.sendFile('C:/Users/rbrbr/Documents/js/angular_with_express/index.html');
-// });
+
+app.get('/signup', (req, res) => {
+  res.sendFile('/static/signup.html', { root: __dirname });
+});
+
+ app.get('/', (req, res) => {
+   res.sendFile('/static/index.html', { root: __dirname });
+ });
+
+ const verify = (req,res,next) =>{
+  
+  next();
+}
+
+ app.post('/',verify,(req,res) => {
+  
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ static/scripts/LoginVerification.js
@@ -0,0 +1,11 @@
+let loginVerifier = angular.module('LoginVerifier',[]);
+loginVerifier.controller('LoginController',function($scope,$http){
+   
+    $scope.checkCredentials = function(){
+        let data = {
+            username: $scope.login.username,
+            password: $scope.login.password
+        };
+        $http.post("http://localhost:3000/",JSON.stringify(data));
+    };
+});
